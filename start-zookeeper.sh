9b0c823229a800f3717859f99825c20612711b61
Adam-Barto <adam.jbarto@gmail.com>
2023-05-01 15:27:44 -0400

    Changed back to arm64 from x86_64 when it became clear it wasn't a factor like I had originally thought

--- start-zookeeper.sh
+++ start-zookeeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MACHARCH=`uname -m`
-MACM1= 'x86_64'# "arm64" Changed due to virtual machine
+MACM1="arm64"
 
 if [[ "$MACHARCH" == "$MACM1" ]]; then
    /opt/homebrew/opt/kafka/bin/zookeeper-server-start /opt/homebrew/etc/kafka/zookeeper.properties
