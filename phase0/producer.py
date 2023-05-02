b3a90210afcab21f7a548f409bcd85df812ab69b
Adam-Barto <adam.jbarto@gmail.com>
2023-05-02 16:57:33 -0400

    Ensure that the right ip is targeted

--- phase0/producer.py
+++ phase0/producer.py
@@ -2,8 +2,10 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-    value_serializer=lambda m: dumps(m).encode('ascii'))
+producer = KafkaProducer(
+    bootstrap_servers=['127.0.0.1:9092'],
+    value_serializer=lambda m: dumps(m).encode('ascii')
+)
 
 for e in range(1000):
     data = {'number' : e}
